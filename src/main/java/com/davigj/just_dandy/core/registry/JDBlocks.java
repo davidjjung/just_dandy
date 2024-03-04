6bbecf7c583afcd167fb2c1f8fc1d89bc242d0e9
DavigJ <david.j.jung@gmail.com>
2024-03-04 14:38:04 -0500

    fixed potted fluffy dandies

--- src/main/java/com/davigj/just_dandy/core/registry/JDBlocks.java
+++ src/main/java/com/davigj/just_dandy/core/registry/JDBlocks.java
@@ -4,9 +4,8 @@ import com.davigj.just_dandy.common.block.FluffyDandelionBlock;
 import com.davigj.just_dandy.core.JustDandy;
 import com.teamabnormals.blueprint.core.util.registry.BlockSubRegistryHelper;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.flag.FeatureFlag;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FlowerPotBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -15,18 +14,23 @@ import net.minecraft.world.level.material.PushReaction;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.util.function.Supplier;
-
 @Mod.EventBusSubscriber(modid = JustDandy.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class JDBlocks {
 	public static final BlockSubRegistryHelper HELPER = JustDandy.REGISTRY_HELPER.getBlockSubHelper();
 
 	public static final RegistryObject<Block> FLUFFY_DANDELION = HELPER.createBlock("fluffy_dandelion", () -> new FluffyDandelionBlock(() -> MobEffects.SATURATION, 12, JustDandyBlockProperties.FLOWER));
-	public static final RegistryObject<Block> POTTED_FLUFFY_DANDELION = HELPER.createBlockNoItem("potted_fluffy_dandelion", () -> new FlowerPotBlock(() -> (FlowerPotBlock) Blocks.FLOWER_POT, FLUFFY_DANDELION, JustDandyBlockProperties.FLOWER_POT));
+	public static final RegistryObject<Block> POTTED_FLUFFY_DANDELION = HELPER.createBlockNoItem("potted_fluffy_dandelion", () -> flowerPot(FLUFFY_DANDELION.get()));
 
 	public static class JustDandyBlockProperties {
 		public static final BlockBehaviour.Properties FLOWER = Block.Properties.of().mapColor(MapColor.PLANT).noOcclusion().noCollission()
 				.instabreak().sound(SoundType.GRASS).offsetType(BlockBehaviour.OffsetType.XZ).pushReaction(PushReaction.DESTROY);
-		public static final BlockBehaviour.Properties FLOWER_POT = Block.Properties.of().instabreak().noOcclusion().pushReaction(PushReaction.DESTROY);
+	}
+
+	private static FlowerPotBlock flowerPot(Block block, FeatureFlag... flags) {
+		BlockBehaviour.Properties blockbehaviour$properties = BlockBehaviour.Properties.of().instabreak().noOcclusion().pushReaction(PushReaction.DESTROY);
+		if (flags.length > 0) {
+			blockbehaviour$properties = blockbehaviour$properties.requiredFeatures(flags);
+		}
+		return new FlowerPotBlock(block, blockbehaviour$properties);
 	}
 }
